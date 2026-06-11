d38e3d1f77891a83716d2469cd61812c972bbf3f
Lucifer-9920 <arsi100001@gmail.com>
2026-06-11 22:09:57 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://botfmbszxqpnelyiccdi.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdGZtYnN6eHFwbmVseWljY2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMDgzMTksImV4cCI6MjA5NjY4NDMxOX0.AMyQ1LlyS58qwqCmAiqFv7xlhL9R_wJP5G48wuR5AHQ";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
