e0796bd366cb5d23ad7f6e2349c852b355db8ac9
Lucifer-9920 <arsi100001@gmail.com>
2026-06-11 06:45:28 +0530

    Implement loading screen and live time update
    Add loading screen functionality and live date/time update.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Mind Number");
+setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("app").style.display = "block";
+}, 2000);
+
+function updateTime() {
+    const now = new Date();
+
+    document.getElementById("liveDateTime").innerHTML =
+        now.toLocaleDateString() +
+        " | " +
+        now.toLocaleTimeString();
+}
+
+setInterval(updateTime, 1000);
+updateTime();
