811a58e69d26adc21f5f0c0e1b95b83eb6003eaa
Lucifer-9920 <arsi100001@gmail.com>
2026-06-11 13:37:42 +0530

    Initialize Supabase client in script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const SUPABASE_URL = "https://botfmbszxqpnelyiccdi.supabase.co";
+const SUPABASE_KEY = "sb_publishable_62Ut3sGstDGcPB0Io3k-dQ_iJaovqHd";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 window.onload = function () {
     document.getElementById("loading").style.display = "none";
     document.getElementById("app").style.display = "block";
