d3505ead23cb7ecd56fe24ed4bc1a073f9e9a544
Lucifer-9920 <arsi100001@gmail.com>
2026-06-11 07:13:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const SUPABASE_URL = "https://botfmbszxqpnelyiccdi.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_62Ut3sGstDGcPB0Io3k-dQ_iJaovqHd";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 setTimeout(() => {
     document.getElementById("loading").style.display = "none";
     document.getElementById("app").style.display = "block";
