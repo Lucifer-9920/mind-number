897407231d39adba47c1f13bccbd385a204c6765
Lucifer-9920 <arsi100001@gmail.com>
2026-06-11 15:17:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,10 +2,9 @@ async function saveNumbers() {
 
     const result = await supabaseClient
         .from("number")
-        .update({ number: parseInt(document.getElementById("n12").value) })
+        .update({ number: 99 })
         .eq("slot", "12PM")
         .select();
 
     alert(JSON.stringify(result));
-
 }
