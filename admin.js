d6e8554f2b801c2e8ea1b2318721bfcf3e1b9e1e
Lucifer-9920 <arsi100001@gmail.com>
2026-06-16 23:02:43 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,16 +9,43 @@ SUPABASE_KEY
 
 async function saveNumbers() {
 
-    alert("Button Working");
 
-    const { data, error } = await supabaseClient
+
+const updates = [
+    { slot: "12PM", value: document.getElementById("n12").value },
+    { slot: "2PM", value: document.getElementById("n14").value },
+    { slot: "4PM", value: document.getElementById("n16").value },
+    { slot: "6PM", value: document.getElementById("n18").value },
+    { slot: "8PM", value: document.getElementById("n20").value },
+    { slot: "10PM", value: document.getElementById("n22").value }
+];
+
+for (const item of updates) {
+
+    if (item.value === "") continue;
+
+    const { error } = await supabaseClient
         .from("number")
-        .select("*");
+        .update({
+            number: parseInt(item.value)
+        })
+        .eq("slot", item.slot);
 
     if (error) {
-        alert("Supabase Error: " + error.message);
-    } else {
-        alert("Supabase Connected");
-        console.log(data);
+        alert("Update Error: " + error.message);
+        return;
     }
+
+    await supabaseClient
+        .from("history")
+        .insert({
+            history_date: new Date().toISOString().split("T")[0],
+            slot: item.slot,
+            number: parseInt(item.value)
+        });
+}
+
+alert("Numbers Saved Successfully!");
+```
+
 }
